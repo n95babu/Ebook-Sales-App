404e84c7f15afb0c73beee15eed6e2b9ddd8d7ed
Nazrul Babu <n95babu@gmail.com>
2019-09-06 16:13:26 -0400

    Add Bootstap

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Set Static Folder
+app.use(express.static(`${__dirname}/public`));
 
 // INDEX ROUTE
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`server is online on port ${port}`);
